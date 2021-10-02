99b83c9796596655a682d8db996ce430407470fe
ArnavPanda <79236877+ArnavPanda@users.noreply.github.com>
2021-10-02 09:47:51 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAW_8hRJqczz7dSfSXP4XW8eT637rZnzd0",
+  authDomain: "storytelling-app-e0959.firebaseapp.com",
+  projectId: "storytelling-app-e0959",
+  storageBucket: "storytelling-app-e0959.appspot.com",
+  messagingSenderId: "560501346170",
+  appId: "1:560501346170:web:34288d6e4af6031fe2d0c3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
